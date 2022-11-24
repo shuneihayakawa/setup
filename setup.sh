e3aa2cc8410a485ea1ef29b0e693d6553a1b3c65
Shunei Hayakawa <shuneihayakawa@shuneihayakawa.biz>
2022-11-23 20:47:58 -0800

    Reorder commands of setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Install homebrew and formulae
+# Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle
 
 # Create symlink to zshrc
 ln -s $PWD/.zshrc ~/.zshrc
+
+# Install homebrew formulae
+brew bundle
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
